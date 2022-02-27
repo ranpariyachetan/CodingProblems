0683cf6bff3aa44e70536dbe2e0bcb32fdc86a1a
ranpariyachetan <ranpariyachetan.ad@gmail.com>
2022-02-28 01:04:57 +0800

    Added solution for Ransomnote problem.

---
+++ CodingProblems/Strings/RansomNote.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+namespace CodingProblems.Strings
+{
+    // Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+    // Each letter in magazine can only be used once in ransomNote.
+
+    public class RansomNote
+    {
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            var map = new Dictionary<char, int>();
+
+            foreach(var c in ransomNote)
+            {
+                if(!map.ContainsKey(c))
+                {
+                    map.Add(c, 0);
+                }
+                map[c]++;
+            }
+
+            foreach(var c in magazine)
+            {
+                if(map.ContainsKey(c))
+                {
+                    map[c]--;
+                    if(map[c] == 0)
+                    {
+                        map.Remove(c);
+                    }
+                }
+            }
+
+            return map.Count == 0;
+        }
+
+        public static void TestCanConstructRansomNote()
+        {
+            var r = "a";
+            var m = "b";
+
+            var result = CanConstruct(r, m);
+
+            Console.WriteLine(result);
+        }
+    }
+}
