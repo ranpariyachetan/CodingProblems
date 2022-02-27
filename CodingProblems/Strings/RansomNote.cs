dde4413398707d38025b24fc98f9d6b2831d78bf
ranpariyachetan <ranpariyachetan.ad@gmail.com>
2022-02-28 01:06:21 +0800

    Added leetcode problem link.

--- CodingProblems/Strings/RansomNote.cs
+++ CodingProblems/Strings/RansomNote.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 namespace CodingProblems.Strings
 {
+    // https://leetcode.com/problems/ransom-note/
     // Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
     // Each letter in magazine can only be used once in ransomNote.
 
