6ec8de421b8970e5c1deae4b13775ce977b95fb0
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2022-02-27 13:33:58 +0800

    Added problem statement for array intersection.

--- CodingProblems/Arrays/Intersections.cs
+++ CodingProblems/Arrays/Intersections.cs
@@ -4,6 +4,10 @@ namespace CodingProblems.Arrays
 {
     public partial class Arrays
     {
+        // https://leetcode.com/problems/intersection-of-two-arrays-ii/
+        // Given two integer arrays nums1 and nums2, return an array of their intersection. 
+        // Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+ 
         private static int[] IntersectArrays(int[] nums1, int[] nums2)
         {
             var result = new List<int>();
