994abcd44878a5a9cb47b209f717312802cd2de9
ranpariyachetan <ranpariyachetan.ad@gmail.com>
2022-02-27 23:26:56 +0800

    Added solution for validating Sudoku board.

---
+++ CodingProblems/Sudoku/Sudoku.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+
+namespace CodingProblems.Sudoku
+{
+    public class Sudoku
+    {
+        private static int count = 0;
+        public static bool IsValidSudoku(char[][] board)
+        {
+            var set = new HashSet<char>();
+            for (var i = 0; i < 9; i++)
+            {
+                count++;
+                var b = CheckDuplicateInRow(board[i]);
+
+                if (!b)
+                    return false;
+
+                b = CheckDuplicateInColumn(board, i);
+
+                if (!b)
+                    return false;
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    count++;
+                    var b = CheckDuplicateInBox(board, i - i % 3, j - j % 3);
+
+                    if (!b)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckDuplicateInRow(char[] arr)
+        {
+            var set = new HashSet<char>();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                count++;
+                if (arr[i] == '.')
+                    continue;
+                if (set.Contains(arr[i]))
+                {
+                    return false;
+
+                }
+                else
+                {
+                    set.Add(arr[i]);
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckDuplicateInColumn(char[][] arr, int colIndex)
+        {
+            var set = new HashSet<char>();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                count++;
+                if (arr[i][colIndex] == '.')
+                    continue;
+                if (set.Contains(arr[i][colIndex]))
+                {
+                    return false;
+                }
+                else
+                {
+                    set.Add(arr[i][colIndex]);
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckDuplicateInBox(char[][] board, int rowStart, int colStart)
+        {
+            var set = new HashSet<char>();
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    count++;
+                    var c = board[i + rowStart][j + colStart];
+
+                    if (set.Contains(c))
+                    {
+                        return false;
+                    }
+                    if (c != '.')
+                    {
+                        set.Add(c);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static void TestValidateSudoku()
+        {
+            var board = new char[][] {
+
+
+new char[] {'5','3','.','.','7','.','.','.','.'},
+new char[] {'6','.','.','1','9','5','.','.','.'},
+new char[] {'.','9','8','.','.','.','.','6','.'},
+new char[] {'8','.','.','.','6','.','.','.','3'},
+new char[] {'4','.','.','8','.','3','.','.','1'},
+new char[] {'7','.','.','.','2','.','.','.','6'},
+new char[] {'.','6','.','.','.','.','2','8','.'},
+new char[] {'.','.','.','4','1','9','.','.','5'},
+new char[] {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            var res = IsValidSudoku(board);
+
+            Console.WriteLine(res);
+            Console.WriteLine(count);
+        }
+    }
+}
