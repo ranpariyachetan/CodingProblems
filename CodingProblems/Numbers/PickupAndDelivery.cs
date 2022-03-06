7b07f0afb2a0e5fa70c133efeafc49e0adad058a
ranpariyachetan <ranpariyachetan.ad@gmail.com>
2022-03-06 12:36:36 +0800

    Added Permutations solution for PickupAndDelivery.

--- CodingProblems/Numbers/PickupAndDelivery.cs
+++ CodingProblems/Numbers/PickupAndDelivery.cs
@@ -1,7 +1,7 @@
 namespace CodingProblems.Numbers
 {
     // https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
-    
+
     // Given n orders, each order consist in pickup and delivery services. 
     // Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i). 
     // Since the answer may be too large, return it modulo 10^9 + 7.
@@ -10,29 +10,29 @@ namespace CodingProblems.Numbers
         private static int MOD = (int)Math.Pow(10, 9) + 7;
         public static int CountOrders(int n)
         {
-            var cache = new int[n + 1,n + 1];
+            var cache = new int[n + 1, n + 1];
 
             return totalWays(n, n, cache);
         }
 
-        private static int totalWays(int unpicked, int undelivered, int[, ] cache)
+        private static int totalWays(int unpicked, int undelivered, int[,] cache)
         {
-            if(unpicked == 0 && undelivered == 0)
+            if (unpicked == 0 && undelivered == 0)
             {
                 return 1;
             }
 
-            if(unpicked < 0 || undelivered == 0 || undelivered < unpicked)
+            if (unpicked < 0 || undelivered == 0 || undelivered < unpicked)
             {
                 return 0;
             }
 
-            if(cache[unpicked, undelivered] != 0)
+            if (cache[unpicked, undelivered] != 0)
                 return cache[unpicked, undelivered];
 
             long ans = 0;
 
-            ans += unpicked * totalWays(unpicked - 1, undelivered,cache);
+            ans += unpicked * totalWays(unpicked - 1, undelivered, cache);
 
             ans %= MOD;
 
@@ -50,6 +50,23 @@ namespace CodingProblems.Numbers
             var n = 3;
 
             Console.WriteLine(CountOrders(n));
+            Console.WriteLine(CountOrdersWithCombinations(n));
+        }
+
+        public static int CountOrdersWithCombinations(int n)
+        {
+            long ans = 1;
+
+            for (var it = 1; it <= n; it++)
+            {
+                ans = ans * it;
+
+                ans = ans * (2 * it - 1);
+
+                ans = ans % MOD;
+            }
+
+            return (int)ans;
         }
     }
 }
