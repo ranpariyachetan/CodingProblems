d947b2854d3197856c0ddffee148357cec560e74
ranpariyachetan <ranpariyachetan.ad@gmail.com>
2022-03-23 23:28:54 +0800

    Added problem statement for discout coupons.

--- CodingProblems/DiscountCoupons.cs
+++ CodingProblems/DiscountCoupons.cs
@@ -4,6 +4,10 @@ namespace CodingProblems
 {
     public static class DiscountCoupons
     {
+        // A discount coupon ([a-z]) is valid if it follows one of the 3 conditions:
+        // An empty string is a valid coupon
+        // A valid coupon with x on either side of it is also valid (xAx is a valid coupon)
+        // A concatentation of 2 valid coupons A and B is a also valid coupon (AB & BA are both valid)
         private  static List<int> CheckValidDiscountCoupons(List<string> discounts)
         {
             List<int> result = new List<int>();
